3683f0ef5bcd1fad11f96b697260ea4a1c5681c5
Naldo Wang <naldo_wang9641@qq.com>
2026-06-09 16:48:08 +0800

    feat: 初始化站点配置，更改为奶片博客
    - 更新站点标题、副标题、描述为奶片博客
    - 更换导航栏 logo 为图标样式
    - 更新站点启动日期为 2026-06-01
    - 关闭友链和赞助页面
    - 添加首篇博文

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,17 +7,17 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "奶片博客",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "一个基于 Astro 和 Fuwari 主题的清新美观个人博客模板",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"奶片博客 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
 
 	// 站点关键词
 	keywords: [
@@ -74,12 +74,12 @@ export const siteConfig: SiteConfig = {
 		// 3. 本地图片（src目录，自动优化但会增加构建时间，推荐）: { type: "image", value: "assets/images/logo.webp", alt: "Logo" }
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
-			type: "image",
-			value: "assets/images/firefly.png",
-			alt: "🍀",
+			type: "icon",
+			value: "material-symbols:home-pin-outline",
+			alt: "🐱",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "奶片博客",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-06-01",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -137,9 +137,9 @@ export const siteConfig: SiteConfig = {
 	// bangumi的数据为编译时获取的，所以不是实时数据，请配置bangumi.userId
 	pages: {
 		// 友链页面开关
-		friends: true,
+		friends: false,
 		// 赞助页面开关
-		sponsor: true,
+		sponsor: false,
 		// 留言板页面开关，需要配置评论系统
 		guestbook: true,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
---
+++ src/content/posts/first-post.md
@@ -0,0 +1,11 @@
+---
+title: 欢迎使用
+published: 2026-06-08
+description: ''
+image: ''
+tags: [文章]
+category: ''
+draft: false
+slug: first-post
+---
+本站使用Firefly主题搭建。
